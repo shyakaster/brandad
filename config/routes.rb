2e9305a8a3fa76d9bc223830c6b1419a986cd21a
alex shyaka <shyakaster@gmail.com>
2016-11-17 16:05:57 +0300

    made links work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts, only:[:new, :create]
-
-  root 'welcome#index'
+    root 'welcome#index'
+    resources :contacts, only:[:new, :create]
 
 end
